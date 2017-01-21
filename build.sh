4313f90c37fe55d2de00cb5e8bb054887de2bc75
Sydney Erickson <sydneyerickson@icloud.com>
2017-01-20 17:10:52 -0800

    Downgrading to GCC 6.2.0 to hopefully avoid strip issues
    strip: invalid option -- c

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ export CLOOG_VERSION=0.18.4
 export MPFR_VERSION=3.1.5
 export MPC_VERSION=1.0.3
 export BINUTILS_VERSION=2.27
-export GCC_VERSION=6.3.0
+export GCC_VERSION=6.2.0
 
 export PROJECT_ROOT=$PWD
 export INSTALL_DIR=$HOME/SydKit.framework
